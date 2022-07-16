5fc82c566af5752de47578e32e4aaf0c8c58cd08
dbadoy4874 <dbadoy4874@gmail.com>
2022-07-16 09:14:31 +0900

    backend: add backend methods

--- backend/backend.go
+++ backend/backend.go
@@ -9,6 +9,24 @@ func NewBackend() *Backend {
 	return &Backend{}
 }
 
-func (b *Backend) Contribution(url string) (types.Contribution, error) {
-	return b.git.contribution(url)
+func (b *Backend) EnrollVerify(enrollInfo types.EnrollInfo) (bool, error) {
+	contributors, err := b.Contributors(enrollInfo.Url)
+	if err != nil {
+		return false, err
+	}
+	for _, contributor := range contributors {
+		if contributor.User == enrollInfo.Subject {
+			return true, nil
+		}
+	}
+	// it's not error. need that result is 'false'
+	return false, nil
+}
+
+func (b *Backend) EnrollInfo(enrollId uint) (types.EnrollInfo, error) {
+	return b.contracts["issuer"].EnrollInfo(enrollId)
+}
+
+func (b *Backend) Contributors(url string) (types.Contributor, error) {
+	return b.git.contributor(url)
 }
