140ce72e9a2d24d50f0c0d8226fb807a107eade0
dbadoy4874 <dbadoy4874@gmail.com>
2022-07-16 09:15:00 +0900

    backend: add contract method, impl not yet

--- backend/contracts.go
+++ backend/contracts.go
@@ -3,3 +3,10 @@ package backend
 // managing interaction to ethereum contract
 type Contract struct {
 }
+
+func (c *Contract) EnrollInfo(user string) (*types.EnrollInfo, error) {
+	// subject, enrollInfo :=
+	// EnrollInfo = enrollInfo
+	// ENrollInfo.SetSubject(subject)
+	return nil, nil
+}
