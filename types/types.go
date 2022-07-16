bc03c013660529054d88b7a9aa2d6f142a852095
dbadoy4874 <dbadoy4874@gmail.com>
2022-07-16 09:17:39 +0900

    types: add type EnrollInfo, rename `Contribution` to `Contributor`

--- types/types.go
+++ types/types.go
@@ -1,8 +1,17 @@
 package types
 
-type Contribution struct {
+type Contributor struct {
 	User          string `json: "login"`
 	AvartarUrl    []byte `json: "avartar_url"`
 	Url           string `json: "html_url"`
 	Contributions uint   `json: "contributions"`
+}
+
+type EnrollInfo struct {
+	Url     string
+	Subject string
+}
+
+func (e *EnrollInfo) SetSuject(user string) {
+	e.Subject = user
 }
