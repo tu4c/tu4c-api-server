565e4218e2ee58f622e2cc687b63a44ab66008d0
dbadoy4874 <dbadoy4874@gmail.com>
2022-07-17 21:52:55 +0900

    backend: add method NewChain()

--- backend/chain.go
+++ backend/chain.go
@@ -1,5 +1,25 @@
 package backend
 
+import "github.com/ethereum/go-ethereum/ethclient"
+
 // managing to ethereum node connection
 type Chain struct {
+	name string
+	url  string
+	eth  *ethclient.Client
 }
+
+func NewChain(name string, url string) (*Chain, error) {
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+	return &Chain{
+		name: name,
+		url:  url,
+		eth:  client,
+	}, nil
+}
+
+func (c *Chain) Name() string { return c.name }
+func (c *Chain) Url() string  { return c.url }
